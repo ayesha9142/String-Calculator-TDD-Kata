dbcd5fd52d998d0fea4020c4b8efba16b7167359
ayesha-fortheye <iamayesha42@gmail.com>
2024-09-05 20:58:01 +0530

    UI & improvements

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,18 +1,30 @@
 export function add(numbers) {
-  if(!numbers) return 0;
+  if (!numbers) return 0;
 
   let delimiters = /[\n,]/;
 
-  if(numbers.startsWith('//')) {
-    const newArray = numbers.split('\n');
+  if (numbers.startsWith("//")) {
+    const newArray = numbers.split("\n");
     delimiters = new RegExp(newArray[0].slice(2));
     numbers = newArray[1];
   }
 
   const numArray = numbers.split(delimiters).map(Number);
-  const negativeNumbers = numArray.filter((num) => num < 0)
-  if(negativeNumbers.length) {
-    throw new Error(`Negative numbers not allowed ${negativeNumbers.join(',')}`);
-  }
+
+  checkNegativeNumbers(numArray);
+
+  return sumNumbers(numArray);
+}
+
+function sumNumbers(numArray) {
   return numArray.reduce((sum, num) => sum + num, 0);
+}
+
+function checkNegativeNumbers(numArray) {
+  const negativeNumbers = numArray.filter((num) => num < 0);
+  if (negativeNumbers.length) {
+    throw new Error(
+      `Negative numbers not allowed ${negativeNumbers.join(",")}`
+    );
+  }
 }
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { add } from "../StringCalculator";
+
+import styles from './HomePage.module.css'
 
 const HomePage = () => {
+  const [value, setValue] = useState("");
+  const [dynamicResult, setDynamicResult] = useState("");
+  const [error, setError] = useState("")
+  const handleCalculate = () => {
+    try {
+        const result = add(value);
+        setDynamicResult(result)
+        setError("")
+    } catch(err) {
+        setError(err.message);
+        setDynamicResult("");
+    }
+  }
   return (
-    <div>
+    <div className={styles.container}>
       <h1>String Calculator TDD Kata</h1>
+      <input
+        type="text"
+        placeholder="Enter numbers:"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={handleCalculate}>Submit</button>
+      {dynamicResult && <div className={styles.result}>Result: {dynamicResult}</div>}
+      {error && <div className={styles.error}>{error}</div>}
     </div>
   );
 };
