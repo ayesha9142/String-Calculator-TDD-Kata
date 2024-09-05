503eae0000aaeb3be6ae7e3613de91530310938f
ayesha-fortheye <iamayesha42@gmail.com>
2024-09-05 18:22:07 +0530

    test for handling of single and multiple numbers

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,5 +1,13 @@
 import { add } from "./StringCalculator";
 
-test('returns 0 for an empty string', () => {
+test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
+});
+
+test("returns the number itself when one number is provided", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns the sum of two numbers", () => {
+  expect(add("1,2")).toBe(3);
 });
